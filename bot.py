ddf159ed9d811bb6cc96704ad61f8c7987711a33
Revcy <63965069+Revcy@users.noreply.github.com>
2022-07-04 00:25:57 +0300

    Added InlineKeyboard for open URL-page

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from re import T
 import telebot as tg
 
 from modules.weather import weather_report
@@ -20,9 +21,13 @@ def weather_function(message):
 def weather_report_function(message):
     language = 'en'
     city = message.text
-
     report = weather_report(city, language)
 
-    bot.send_message(message.chat.id, report)
+    if report != None:
+        weather_key = tg.types.InlineKeyboardMarkup(row_width=1)
+        button = tg.types.InlineKeyboardButton(text='Detailed report', url=f'https://openweathermap.org/city/{report[9]}')
+        weather_key.add(button)
+
+        bot.send_message(message.chat.id, f'Weather info for {report[0]}, {report[1]}: \n\nTime: {report[2]} \nDescription: {report[3]} \nTemperature: {report[4]}°C (Feels like: {report[5]}°C) \nWing speed: {report[6]} m/s \nPressure: {report[7]} Pa \nHumidity: {report[8]}%', reply_markup=weather_key)
 
 bot.polling(non_stop=True)
