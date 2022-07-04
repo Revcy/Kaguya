55fba08f4ab00c45120876643162262adf67a0aa
Revcy <63965069+Revcy@users.noreply.github.com>
2022-07-04 14:09:29 +0300

    Updated file reading for tokens

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-from re import T
 import telebot as tg
 
 from modules.weather import weather_report
 
-with open('database/token.txt', encoding='utf8') as file:
-    token = file.readline()
+with open('database/tokens.txt', encoding='utf8') as file:
+    tokens = file.readlines()
+    token, appid_openweathermap = tokens[0].replace('\n', ''), tokens[1].replace('\n', '')
     file.close()
 
     bot = tg.TeleBot(token)
@@ -21,13 +21,13 @@ def weather_function(message):
 def weather_report_function(message):
     language = 'en'
     city = message.text
-    report = weather_report(city, language)
+    report = weather_report(city, language, appid_openweathermap)
 
     if report != None:
         weather_key = tg.types.InlineKeyboardMarkup(row_width=1)
         button = tg.types.InlineKeyboardButton(text='Detailed report', url=f'https://openweathermap.org/city/{report[9]}')
         weather_key.add(button)
-
+        
         bot.send_message(message.chat.id, f'Weather info for {report[0]}, {report[1]}: \n\nTime: {report[2]} \nDescription: {report[3]} \nTemperature: {report[4]}°C (Feels like: {report[5]}°C) \nWing speed: {report[6]} m/s \nPressure: {report[7]} Pa \nHumidity: {report[8]}%', reply_markup=weather_key)
 
 bot.polling(non_stop=True)
